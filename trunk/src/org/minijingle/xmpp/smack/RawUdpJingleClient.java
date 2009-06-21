4186c684425402944b09097cb5c690569a34be20
barata7 <barata7@e56366ba-5a6f-11de-8832-3f4c773c73ae>
2009-06-21 08:38:49 +0000

    First Official Version

---
+++ trunk/src/org/minijingle/xmpp/smack/RawUdpJingleClient.java
@@ -0,0 +1,46 @@
+package org.minijingle.xmpp.smack;
+
+import org.jivesoftware.smack.provider.ProviderManager;
+import org.minijingle.jingle.Jingle;
+import org.minijingle.jingle.transport.Candidate;
+import org.minijingle.jingle.transport.RawUdpTransport;
+import org.minijingle.media.PJMediaManager;
+import org.minijingle.network.LocalIPResolver;
+
+public class RawUdpJingleClient extends Client {
+
+    private RawUdpCallManager callManager;
+    private final String jingleProxy;
+
+    public RawUdpJingleClient(final String username, final String password, final String server, final String jingleProxy) {
+        super(username, password, server);
+        this.jingleProxy = jingleProxy;
+        login();
+    }
+
+    protected void loggedIn() {
+
+        JingleProvider.enableJingle(connection);
+
+        this.callManager = new RawUdpCallManager(connection, new PJMediaManager(), new RawUdpTransport(new Candidate(LocalIPResolver.getLocalIP(), "10002", "0")), jingleProxy);
+
+    }
+
+    protected void loggedOut() {
+
+    }
+
+    public static void main(String args[]) {
+
+        new RawUdpJingleClient(args[1], args[2], args[3], args[4]);
+
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
